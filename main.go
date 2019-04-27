8da66b145fe57047516aa5f1179cd43c4f92f096
kawakami <kawakami.ozone@gmail.com>
2019-04-28 02:44:25 +0900

    hellow world server

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./"))))
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
